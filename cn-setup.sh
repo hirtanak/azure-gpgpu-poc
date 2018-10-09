f8e658458ad01c25ec1002a8a8b6122fe3acf745
hirtanak <hirtanak@gmail.com>
2018-10-10 07:37:58 +0900

    update nfs parameter

--- cn-setup.sh
+++ cn-setup.sh
@@ -15,9 +15,9 @@ if [ $FLAG = NOTMOUNTED ] ; then
     pkill -9 yum
     sleep 10
     yum install -y -q nfs-utils pdsh
-    mkdir -p /mnt/nfsshare
-    mkdir -p /mnt/resource/scratch
-    chmod 777 /mnt/nfsshare
+    mkdir -p /data/scratch
+    chmod 777 /data/scratch
+    chown -R $USER:$USER /data/$USER/
     systemctl enable rpcbind
     systemctl enable nfs-server
     systemctl enable nfs-lock
@@ -27,30 +27,26 @@ if [ $FLAG = NOTMOUNTED ] ; then
     systemctl start nfs-lock
     systemctl start nfs-idmap
     localip=`hostname -i | cut --delimiter='.' -f -3`
-    ## echo "$IPPRE:/mnt/nfsshare    /mnt/nfsshare   nfs defaults 0 0" | tee -a /etc/fstab
-    echo "$IPPRE:/mnt/resource/scratch    /mnt/resource/scratch   nfs   defaults 0 0" | tee -a /etc/fstab
+    echo "$IPPRE:/data/scratch    /data/scratch   nfs   defaults 0 0" | tee -a /etc/fstab
     mount -a
     df | grep $IPPRE
     impi_version=`ls /opt/intel/impi`
     source /opt/intel/impi/${impi_version}/bin64/mpivars.sh
     ln -s /opt/intel/impi/${impi_version}/intel64/bin/ /opt/intel/impi/${impi_version}/bin
     ln -s /opt/intel/impi/${impi_version}/lib64/ /opt/intel/impi/${impi_version}/lib
-    
-    #echo "@reboot mkdir -p /mnt/resource/scratch && chown ${USER}:${USER} /mnt/resource/scratch && mount -t nfs $IPPRE:/mnt/resource/scratch /mnt/resource/scratch" | tee -a /var/spool/cron/root
-    #echo "@reboot chown ${USER}:${USER} /mnt/resource/scratch" | tee -a /var/spool/cron/root
-    #echo "@reboot $IPPRE:/mnt/resource/scratch /mnt/resource/scratch" | tee -a /var/spool/cron/root
-    
+        
     # fix mount problem
     echo "@reboot ~/bootcron.sh" | tee -a /var/spool/cron/root
     echo "*/15 * * * * ~/bootcron.sh" | tee -a /var/spool/cron/root
+    
     # create script
     echo "#!/bin/bash" | tee -a ~/bootcron.sh
-    echo "if [ -d /mnt/resource/scratch ]; then" | tee -a ~/bootcron.sh
+    echo "if [ -d /data/scratch ]; then" | tee -a ~/bootcron.sh
     echo "echo '[bootcron] Already mounted'" | tee -a ~/bootcron.sh
     echo "else" | tee -a ~/bootcron.sh
-    echo "mkdir -p /mnt/resource/scratch" | tee -a ~/bootcron.sh
-    echo "chown ${USER}:${USER} /mnt/resource/scratch" | tee -a ~/bootcron.sh
-    echo "mount -t nfs $IPPRE:/mnt/resource/scratch /mnt/resource/scratch" | tee -a ~/bootcron.sh
+    echo "mkdir -p /data/scratch" | tee -a ~/bootcron.sh
+    echo "chown ${USER}:${USER} /data/scratch" | tee -a ~/bootcron.sh
+    echo "mount -t nfs $IPPRE:/data/scratch /data/scratch" | tee -a ~/bootcron.sh
     echo "fi" | tee -a ~/bootcron.sh
     chmod +x ~/bootcron.sh
     ln -s /root/bootcron.sh /home/${USER}/bootcron.sh
